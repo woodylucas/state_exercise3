f4f08d9942761562f59dca3d7acf9c791abad894
woodylucas <wlucas0311@gmail.com>
2020-11-01 21:35:56 -0500

    added helper file

--- src/Box.js
+++ src/Box.js
@@ -1,19 +1,26 @@
 import React, { Component } from 'react'
+import {choice} from './helpers'; 
 import './Box.css';
 
 class Box extends Component {
+    static defaultProps = {
+        allColors: ['purple', 'magenta', 'lilac', 'pink']
+    };
     constructor(props) {
         super(props)
-        this.state = {color: 'red'}; 
+        this.state = { color: choice(this.props.allColors) }; 
         this.handleClick = this.handleClick.bind(this);
     }
 
-    handleClick() {
-
-    }
+    handleClick() {}
     render() {
         return (
-            <div className="Box" style={{backgroundColor: this.state.color}} onClick={this.handleClick}></div>
+            <div 
+                className="Box" 
+                style={{backgroundColor: this.state.color}} 
+                onClick={this.handleClick}
+             >
+            </div>
         )
     }
 }
