c6db2242ba32bd84b35c247552aff24d9960c5b8
woodylucas <wlucas0311@gmail.com>
2020-11-02 17:03:07 -0500

    added prettier to format code

--- src/Box.js
+++ src/Box.js
@@ -1,37 +1,35 @@
-import React, { Component } from 'react'
-import {choice} from './helpers'; 
+import React, { Component } from 'react';
+import { choice } from './helpers';
 import './Box.css';
 
 class Box extends Component {
+	constructor(props) {
+		super(props);
 
-    constructor(props) {
-        super(props)
-        this.state = { color: choice(this.props.colors) }; 
-        this.handleClick = this.handleClick.bind(this);
-    }
+		this.state = { color: choice(this.props.colors) };
+		this.handleClick = this.handleClick.bind(this);
+	}
 
-    pickColor() {
-        let newColor; 
-        do {
-            newColor = choice(this.props.colors);
-        }  while (newColor === this.state.color)
-        this.setState({ color: newColor }); 
-    }
+	pickColor() {
+		let newColor;
+		do {
+			newColor = choice(this.props.colors);
+		} while (newColor === this.state.color);
+		this.setState({ color: newColor });
+	}
 
-    handleClick() {
-        this.pickColor(); 
-    }
-    render() {
-        return (
-            <div 
-                className="Box" 
-                style={{backgroundColor: this.state.color}} 
-                onClick={this.handleClick}
-             >
-            </div>
-        )
-    }
+	handleClick() {
+		this.pickColor();
+	}
+	render() {
+		return (
+			<div
+				className='Box'
+				style={{ backgroundColor: this.state.color }}
+				onClick={this.handleClick}
+			/>
+		);
+	}
 }
 
-
-export default Box; 
+export default Box;
